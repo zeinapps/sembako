8dc92dac3679662d41ce006b0bc4579c272db686
zein <zein@jayantara.co.id>
2017-01-19 16:28:00 +0700

    404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,14 +3,11 @@
 
 <section>
     <div class="container text-center">
-        <div class="logo-404">
-            <a href="/"><span>Toko Online</span></a>
-        </div>
+        
         <div class="content-404">
-            <img src="/eshop/images/404/404.png" class="img-responsive" alt="" />
             <h1><b>Maaf!</b> Halaman tidak Ditemukan</h1>
             <p>Hubungi Kami jika Anda memerlukan bantuan</p>
-            <h2><a href="/">Kembali ke Beranda</a></h2>
+            <h2><a href="/">Kembali ke Beranda</a></h2><br>
         </div>
     </div>
 
